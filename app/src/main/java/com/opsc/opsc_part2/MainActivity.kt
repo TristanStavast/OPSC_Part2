cfbc830bd2524b93f9a428077d1a32c03859ce86
Tristan Stavast <stavasttristan@gmail.com>
2024-09-25 13:05:09 +0200

    Added Login, Register and Dashboard pages

--- app/src/main/java/com/opsc/opsc_part2/MainActivity.kt
+++ app/src/main/java/com/opsc/opsc_part2/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.opsc.opsc_part2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +14,15 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val btnlogin: Button = findViewById(R.id.btnLogin)
+        val btnregister : Button= findViewById(R.id.btnRegister)
+        btnlogin.setOnClickListener {
+            var int = Intent(this, Dashboard::class.java)
+            startActivity(int)
+        }
+        btnregister.setOnClickListener {
+            var int = Intent(this, Register::class.java)
+            startActivity(int)
+        }
     }
 }
