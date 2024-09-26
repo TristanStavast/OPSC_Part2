a9e3506d426daf93411994232c50205e349e81e0
AlexiGiagas <alexi.giagas@gmail.com>
2024-09-26 20:22:22 +0200

    Changed background image for all pages made settings button on dashboard work

--- app/src/main/java/com/opsc/opsc_part2/Dashboard.kt
+++ app/src/main/java/com/opsc/opsc_part2/Dashboard.kt
@@ -3,6 +3,8 @@ package com.opsc.opsc_part2
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,16 +16,19 @@ class Dashboard : BaseActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_dashboard)
 
-//        val btnsettings = findViewById<Button>(R.id.btnSettings)
-//
-//        btnsettings.setOnClickListener {
-//            val int = Intent(this, Settings::class.java)
-//            startActivity(int)
-//        }
+        val imgsettings = findViewById<ImageView>(R.id.imgSettingsDash)
+        val txtwelcome = findViewById<TextView>(R.id.txtWelcome)
 
-        val btnlights : Button = findViewById(R.id.lightsButton)
-        val btndoors : Button = findViewById(R.id.doorsButton)
-        val btnsensors : Button = findViewById(R.id.sensorsButton)
+        imgsettings.setOnClickListener {
+            val int = Intent(this, Settings::class.java)
+            startActivity(int)
+        }
+
+        txtwelcome.setText("Welcome back, " /* add user firstname from database */)
+
+        val btnlights : Button = findViewById(R.id.btnLights)
+        val btndoors : Button = findViewById(R.id.btnDoors)
+        val btnsensors : Button = findViewById(R.id.btnSensors)
 
         btnlights.setOnClickListener {
             val int = Intent(this, Lights::class.java)
